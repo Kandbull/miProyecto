21381f17bcdffd8167f0cf339ecc717231fb47e9
Fabián Alexis Calderón Pérez <sabo0p731@gmail.com>
2023-09-14 00:20:14 -0300

    breck v1.3
    - cambiando cosas de settings intentando colocar un sistema de dark theme
    - en calendario viendo como colocar los días feriados
    - coloreando 18 y 19 de sep como prueba

--- src/app/tabs/calendar/calendar.page.ts
+++ src/app/tabs/calendar/calendar.page.ts
@@ -20,20 +20,32 @@ export class CalendarPage implements OnInit {
 
     // esto hace que cada quinto dia tenga un color azulado
     // esto por el hecho de tener un color primario
-    if (utcDay % 5 === 0) {
+    /*if (utcDay % 5 === 0) {
       return {
         textColor: '#800080',
         backgroundColor: '#ffc0cb',
       };
+    }*/
+
+    if (utcDay % 18 === 0 ){
+      return{
+        textColor: '#DC001A'
+      };
+    }
+
+    if (utcDay % 19 === 0 ){
+      return{
+        textColor: '#DC001A'
+      };
     }
     // esto hace que cada tercer dia tenga un color rojizo
     // que este es un color secundario
-    if (utcDay % 3 === 0) {
+    /*if (utcDay % 3 === 0) {
       return {
         textColor: 'var(--ion-color-secondary-contrast)',
         backgroundColor: 'var(--ion-color-secondary)',
       };
-    }
+    }*/
 
     return undefined;
   };
--- src/app/tabs/settings/settings.page.ts
+++ src/app/tabs/settings/settings.page.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
 })
+
 export class SettingsPage implements OnInit {
 
   constructor() { }
@@ -13,3 +14,28 @@ export class SettingsPage implements OnInit {
   }
 
 }
+
+const toggle = document.querySelector('#themeToggle');
+/*
+toggle.addEventListener('ionChange', (ev) => 
+  {
+    document.body.classList.toggle('dark', ev.);
+  }
+);
+
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+// Listen for changes to the prefers-color-scheme media query
+prefersDark.addListener((e) => checkToggle(e.matches));
+
+// Called when the app loads
+function loadApp() {
+  checkToggle(prefersDark.matches);
+}
+
+// Called by the media query to check/uncheck the toggle
+function checkToggle(shouldCheck) {
+  toggle.checked = shouldCheck;
+}
+
+*/
